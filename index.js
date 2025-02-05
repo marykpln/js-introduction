97f9647e65bd3b4c7b774ed080e4b1330ed61774
MariiaKpln <kpln.mary@gmail.com>
2025-02-05 23:01:48 +0200

    number math functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hell")
+// let a=2;
+// a ="abc";
+// console.log(a);
+// const b = 3;
+//b = 5; const can't be updated
+//primitive tipes
+/*****
+ * 1. number
+ * 2.string
+ * 3. boolean (true, false)
+ * 4. undefined
+ * 5.null
+ */
+
+// 1.number
+let a = 100;
+a /= 3;
+console.log("non-normilized result of division", a);
+a = Math.trunc(a);
+console.log("normilized result of division", a);
+a = Math.round(30.6);
+a = Math.floor(a);
+a = Math.ceil(a);
