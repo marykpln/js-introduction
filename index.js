a33c590b455d8fe6fe11c5d0dd5905914169420b
MariiaKpln <kpln.mary@gmail.com>
2025-02-12 20:18:57 +0200

    func as an object

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Helloooooo")
+//function is an objectt with operator () (function call)
+//conversion func to str will return str containing text of the func
+//conversion fun to num will return NaN
+
+const sum = function (a, b) {
+  return a + b;
+};
+
+const substract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a - b;
+};
+// console.log(sum + "");
+
+function calculate(fun, a, b) {
+  return fun(a, b);
+}
+console.log(calculate(substract, 10, 5));
