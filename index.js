1e9a16863f61e1d48d3cd9a9a44517dc1dc5695a
MariiaKpln <kpln.mary@gmail.com>
2025-02-16 20:34:43 +0200

    variatic parametrs/spread operator

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-//function is an objectt with operator () (function call)
-//conversion func to str will return str containing text of the func
-//conversion fun to num will return NaN
-
-const sum = function (a, b) {
-  return a + b;
-};
-
-const substract = function (a, b) {
-  return a - b;
-};
-
-const multiply = function (a, b) {
-  return a - b;
-};
-// console.log(sum + "");
-
-function calculate(fun, a, b) {
-  return fun(a, b);
+function sum(...operands) {
+  let sum = 0;
+  // if (Array.isArray(operands[0])) {
+  //   operands = [...operands];
+  // }
+  for (let i = 0; i < operands.length; i++) {
+    let operand = operands[i];
+    if (Array.isArray(operand)) {
+      operand = sumArray(operand);
+    }
+    sum = sum + operand;
+  }
+  return sum;
 }
-console.log(calculate(substract, 10, 5));
+
+function sumArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    let sum = 0;
+    sum += array[i];
+  }
+  return sum;
+}
+// // console.log(`sum(1,2,3)=${sum(1,2,3)}`);
+// console.log(`sum([1,2,3], 4, 6)=${sum([1,2,3], 5, 6)}`);
+
+console.log(`Math.max(1,2,3) = ${Math.max(1, 2, 3)}`);
+
+console.log(`Math.max(...[1,2,3]) = ${Math.max(...[1, 2, 3])}`);
