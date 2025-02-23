ce77afd9ae6362fb1773757d185921f8a93d169b
MariiaKpln <kpln.mary@gmail.com>
2025-02-23 21:29:40 +0200

    some/every/reduce/slice and resume for applying functions

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
-const array = ["HELLO", 122, -10, "Java", "JavaScript", 500, "Node.js"];
-//finding
-let index = array.indexOf("Java");
-console.log(index);
-const array1 = array.filter(function (el) {
-  let result = true;
-  if (typeof el == "string") {
-    result = !el.includes("Java");
-  }
-  return result;
-});
-console.log(array1);
+let array = ["HELLO", 122, -10, "Java", "JavaScript", 500, "Nodejs"];
+let array1 = array.slice(); //arra1 contains copy of array
+function isNumber(element) {
+  return typeof element == "number";
+}
 
-//forEach
-array.forEach(function (el, index) {
-  console.log(`${i + 1} : ${el}`);
-});
-
-//map
-const array2 = array.map(function (e) {
-  let res = typeof e == "number" ? Math.abs(e).toString().length : e.length;
+function orderedList(array) {
+  //array is any array containing both numbewrs and strings
+  //returns HTML text for ordered list
+  //string will have default color
+  //number will have red color
+  //<ol><li>....</li>...<li>....</li></ol>
+  //example (only ewxample)
+  const items = getItems(array).join(" ");
+  return `<ol>
+       ${items}
+    </ol>`;
+}
+function getItems(array) {
+  const res = array.map(getItem);
   return res;
-});
-
-console.log(array2);
-
-//join
-const str = array.join("<br>");
-
-//sort
-const array3 = [122, 10000, 2, 9, 1000000000];
-// console.log(array3.sort());
-// console.log(array3);
-// const array4 = [122, 10000, 2, 9, 1000000000];
-// console.log(array4.toSorted());
-// console.log(array4);
-
+}
+function getItem(element) {
+  const res = `<li class="item ${
+    isNumber(element) ? "item_number" : ""
+  }">${element}</li>`;
+  return res;
+}
+array = array.filter((e) => isNumber(e));
 console.log(
-  array1.toSorted(function (a, b) {
-    return a - b;    //b-a
-  })
+  "array.some(a => a < 0) =>",
+  array.some((a) => a < 0)
 );
+console.log(
+  "array.every(a => a < 0) =>",
+  array.every((a) => a < 0)
+);
+console.log(
+  `sum of array [${array}] => `,
+  array.reduce((acc, cur) => acc + cur, 0)
+);
+console.log(
+  `minimal value of array [${array}] =>`,
+  array.reduce((acc, cur) => (acc < cur ? acc : cur))
+);
+array = array1.slice(1, 3); //array containing two elements 122, -10 - result see on the browser
+
+const bodyElement = document.querySelector("body");
+bodyElement.innerHTML = orderedList(array);
+//resume of the methods based on array processing with callback function
+//forEach - callback function doesn't return anything but performs some action with each element of array
+//map - callback function returns converted one element. Returns new array with converted elements by callback function
+//filter - callback is a predicate function. Returns new array with the elements matching callback function
+//some - callback is a predicate function. Returns true if at least one element matches callback function
+//every - callback is a predicate function. Returns true if all elements match callback function
+//reduce - callback is a function returning some accumulatoring value
