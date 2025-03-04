6296940047eaed4ab899f89f0ca1bdff27e8d029
MariiaKpln <kpln.mary@gmail.com>
2025-03-04 19:01:13 +0200

    hw done

--- testframework.js
+++ testframework.js
@@ -1,3 +1,5 @@
+import { isAnagram } from "./anagram.js";
+
 function test(commonScript, testObj) {
   //testObj structure {script: <string containg script text>, expected: <any type>}
   //returns resultObj with structure {script: <string containg script text>,
@@ -24,7 +26,12 @@ function test(commonScript, testObj) {
 function createTestResult(script, expectedJSON, actualJSON, result) {
   return { script, expectedJSON, actualJSON, result };
 }
-export function testframework(testName, commonScript, scripts, expectedResults) {
+export function testframework(
+  testName,
+  commonScript,
+  scripts,
+  expectedResults
+) {
   //input
   //scripts - array of tested scripts
   //expectedResults - array of appropriate results
