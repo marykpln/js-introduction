12481add4e8dedb2e65f2871ade8de1300881fbe
MariiaKpln <kpln.mary@gmail.com>
2025-03-04 18:57:28 +0200

    test anagram func

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Helloooooo")
+import { testframework } from "./testframework.js";
+
+testframework(
+  "isAnagram testing",
+  'const string = "hello"',
+  [
+    'isAnagram(string, "olleh")',
+    'isAnagram(string, "lehol")',
+    'isAnagram(string, "ollle")',
+    'isAnagram(string, "hell")',
+    'isAnagram(string, "oelhl")',
+  ],
+  [true, true, false, false, true]
+);
