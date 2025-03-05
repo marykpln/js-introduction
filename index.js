936394834a28b21d0c5452f70c5f41de350cfa79
MariiaKpln <kpln.mary@gmail.com>
2025-03-05 20:14:43 +0200

    hw19 task1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Helloooooo")
+const array = [1, 2, 3];
+// const temp = second;
+// second = first;
+// first = second;
+
+// let first = 1;
+// let second = 2;
+let [first, second] = array;
+console.log(`second = ${second}`);
