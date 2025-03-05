95e28bd1f8362e601b6daccfe81ebec71b2f27e7
MariiaKpln <kpln.mary@gmail.com>
2025-03-05 20:25:37 +0200

    object setting with spread and without

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ const array = [1, 2, 3];
 // let second = 2;
 let [first, second] = array;
 console.log(`second = ${second}`);
+console.log(`first = ${first}`);
+
+const person1 = { name: "Voldemort", age: 25 };
+//person2 is same as person1
+// const person2 = person1;
+const person2 = { ...person1 };
+console.log(
+  `before setting  age to 30`,
+  `person1.age =${person1.age}; peson2.age =${person2.age}`
+);
+person1.age = 30;
+console.log(
+  `after setting  age to 30`,
+  `person1.age =${person1.age}; peson2.age =${person2.age}`
+);
