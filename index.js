9a164bce1789ed0043ab471874ae6ed2571f844e
MariiaKpln <kpln.mary@gmail.com>
2025-03-08 22:28:57 +0200

    firsttask

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-import { testframework } from "./testframework.js";
-testframework(
-  "mergeObjects testing",
-  "const obj1={x:2, y:3}; const obj2={z:1, i:-10}; const obj3 = {x:10, y:30}",
-  [
-    "mergeObjects(obj1, obj2, true)",
-    "mergeObjects(obj1,obj3,true)",
-    "mergeObjects(obj1,obj3,false)",
-  ],
-  [
-    { x: 2, y: 3, z: 1, i: -10 },
-    { x: 2, y: 3 },
-    { x: 10, y: 30 },
-  ]
-);
+const array = [1, 2, 3];
+let [first, second] = array;
+console.log(`second = ${second}`);
+[second, first] = array;
+
+console.log(`second after = ${second}`);
+console.log(`first after = ${first}`);
