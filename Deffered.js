9bbbde9d2ca938e997599cb57eb9149a2bb6ce2e
MariiaKpln <kpln.mary@gmail.com>
2025-03-15 23:42:21 +0200

    task 1

---
+++ Deffered.js
@@ -0,0 +1,30 @@
+export default class Deferred {
+  constructor() {
+    this.result = "hello";
+    this.num = 1;
+    this.callbacks = [];
+  }
+  numberToLetter() {
+    return this.num >= 1 && this.num <= 26
+      ? String.fromCharCode(96 + this.num)
+      : null;
+  }
+  changer() {
+    console.log(this.num, this.result);
+    let result = this.numberToLetter();
+    this.num++;
+    return result;
+  }
+  then(callback) {
+    return this.result !== null
+      ? (this.result = callback(this.changer()))
+      : this.callbacks.push(callback);
+  }
+  resolve(arg) {
+    this.result = arg;
+    this.callbacks.forEach((callback) => {
+      this.result = callback(this.changer());
+    });
+    this.callbacks = [];
+  }
+}
--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-import { testframework } from "./testframework.js";
+// import { testframework } from "./testframework.js";
+import Deffered from "./Deffered.js";
 
-class X {
-  constructor(arr) {
-    this.arr = arr;
-  }
-  logElements() {
-    this.arr.forEach((el) => console.log(el));
-  }
-  sumElements() {
-    return this.arr.reduce((acc, cur) => acc + cur);
-  }
-  action(fun) {
-    return fun.call(this);
-  }
-  joinElements() {
-    return this.arr.join();
-  }
-  actionBind(fun) {
-    return fun();
-  }
-}
+const d = new Deffered();
+d.then(function (res) {
+  console.log("1 ", res);
+  return "a";
+});
+d.then(function (res) {
+  console.log("2 ", res);
+  return "b";
+});
+d.then(function (res) {
+  console.log("3 ", res);
+  return "c";
+});
+d.resolve("hello");
 
-const objX = new X([1, 2, 3]);
-// objX.logElements();
-// console.log(objX.sumElements());
-// console.log(objX.action(objX.logElements));
-// console.log(
-//   objX.action(function () {
-//     return this.arr.join();
-//   })
-// );
-// console.log(objX.action(objX.joinElements))
-console.log(objX.actionBind(objX.sumElements.bind(objX)));
+// let charCode = String.fromCharCode("fff");
+// console.log("code", charCode);
