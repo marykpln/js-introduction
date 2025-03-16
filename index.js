23103541b0379c17e249c6a1efc8ca1207688f89
MariiaKpln <kpln.mary@gmail.com>
2025-03-16 19:51:19 +0200

    func stack with example error

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("It's working");
+function getUserPassword(probRightPass) {
+  const passwords = ["correct", "wrong"];
+  const index = Math.random() < probRightPass ? 0 : 1;
+  return passwords[index];
+}
+
+function login(password) {
+  if (password !== "correct") {
+    throw "wrong";
+  }
+  return users[username];
+}
+
+function getUserData(username) {
+  const users = {
+    Vasya: { name: "Vasya", age: 30 },
+    Petya: { name: "Petya", age: 40 },
+  };
+  if (!users[username]) {
+    throw `username ${username} not exist`;
+  }
+}
+
+function funStackExample(username) {
+  const password = getUserPassword(0.8);
+  try {
+    login(password);
+    const userData = getUserData(username);
+    console.log(userData);
+  } catch (error) {
+    console.log(`error: ${error}`);
+  }
+}
+
+funStackExample("Vasya");
