6f009cdd4c0ae0bcc867035f2b7a5e53d877437a
MariiaKpln <kpln.mary@gmail.com>
2025-03-16 20:12:02 +0200

    synchronous sleep method

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-function getUserPassword(probRightPass) {
-  const passwords = ["correct", "wrong"];
-  const index = Math.random() < probRightPass ? 0 : 1;
-  return passwords[index];
-}
-
-function login(password) {
-  if (password !== "correct") {
-    throw "wrong";
-  }
-  return users[username];
-}
-
-function getUserData(username) {
-  const users = {
-    Vasya: { name: "Vasya", age: 30 },
-    Petya: { name: "Petya", age: 40 },
-  };
-  if (!users[username]) {
-    throw `username ${username} not exist`;
+function sleep(timeout) {
+  let runnig = true;
+  const timestamp = new Date().getTime();
+  while (running) {
+    let current = new Date().getTime();
+    if (current - timestamp >= timeout) {
+      runnig = false;
+    }
   }
 }
-
-function funStackExample(username) {
-  const password = getUserPassword(0.8);
-  try {
-    login(password);
-    const userData = getUserData(username);
-    console.log(userData);
-  } catch (error) {
-    console.log(`error: ${error}`);
-  }
-}
-
-funStackExample("Vasya");
+sleep(5000);
+console.log("Hello, world");
