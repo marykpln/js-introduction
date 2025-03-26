4700ef3f6292cf6eb389bddac1f872cda6a6c182
MariiaKpln <kpln.mary@gmail.com>
2025-03-26 18:00:41 +0200

    timeout

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function sleep(timeout) {
-  let runnig = true;
-  const timestamp = new Date().getTime();
-  while (running) {
-    let current = new Date().getTime();
-    if (current - timestamp >= timeout) {
-      runnig = false;
-    }
-  }
+  const rejectData = 5;
+  return new Promise(
+    (resolve) => {
+      setTimeout(() => resolve(), timeout);
+    },
+    (reject) => reject("timeout cannot be negative")
+  );
 }
-sleep(5000);
+const timeout = 5000;
+sleep(timeout)
+  .then(() => console.log(`result after ${timeout * 1000} second`))
+  .catch((e) => console.log(e))
+  .finally(() => console.log(`printing out in any case`));
 console.log("Hello, world");
