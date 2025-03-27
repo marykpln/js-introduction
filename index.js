ccbd471ba840458533df43259c9887e37214ab8f
MariiaKpln <kpln.mary@gmail.com>
2025-03-27 14:10:37 +0200

    asynchronous sleep

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function sleep(timeout) {
-  const rejectData = 5;
-  return new Promise(
-    (resolve) => {
-      setTimeout(() => resolve(), timeout);
-    },
-    (reject) => reject("timeout cannot be negative")
-  );
+  return new Promise((resolve) => {
+    if (timeout < 0) {
+      throw "timeout cannot be negative";
+    }
+    setTimeout(() => resolve(), timeout);
+  });
 }
-const timeout = 5000;
+const timeout = -5000;
 sleep(timeout)
-  .then(() => console.log(`result after ${timeout * 1000} second`))
-  .catch((e) => console.log(e))
-  .finally(() => console.log(`printing out in any case`));
-console.log("Hello, world");
+  .then(() => console.log(`result after ${timeout / 1000} seconds`))
+  .catch((e) => {
+    console.log(e);
+  })
+  .finally(() => console.log("printing out in any case"));
+console.log("Hello world");
